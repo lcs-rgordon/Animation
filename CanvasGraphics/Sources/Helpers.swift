006623339936a53572d6399ca437a0fa5ad59b20
Russell Gordon <rgordon@lcs.on.ca>
2018-11-12 15:12:27 -0500

    Separated large file (Helper.swift) into smaller ones

---
+++ CanvasGraphics/Sources/Helpers.swift
@@ -0,0 +1,26 @@
+//
+//  Helpers.swift
+//  CanvasGraphics
+//
+//  A variety of useful functions.
+//
+//  Created by Russell Gordon on 11/12/18.
+//  Copyright © 2018 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Returns a random number in the given range.
+ 
+ - parameter from: The lowest possible random value that may be returned.
+ - parameter toButNotIncluding: The random number returned will never include this value.
+ 
+ */
+public func random(from : Int, toButNotIncluding : Int) -> Int {
+    
+    let max = UInt32(toButNotIncluding - from)
+    
+    return Int(arc4random_uniform(max)) + from
+    
+}
