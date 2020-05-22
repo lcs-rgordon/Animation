efabd0a9110303fd6c9785cc4d97fd8a93a0855c
Russell Gordon <rgordon@lcs.on.ca>
2020-05-22 17:25:32 -0400

    Basic example from tutorial is complete.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -9,12 +9,21 @@ class Sketch : NSObject {
     
     // Tortoise to draw with
     let turtle : Tortoise
+    
+    // L-system state
+    let axiom : String
+    var word : String
+    let length : Int = 10
+    let angle : Degrees = 60
         
     // This function runs once
     override init() {
         
         // Create canvas object – specify size
         canvas = Canvas(width: 500, height: 500)
+        
+        // Draw slowly
+        canvas.framesPerSecond = 1
         
         // Create turtle to draw with
         turtle = Tortoise(drawingUpon: canvas)
@@ -23,7 +32,42 @@ class Sketch : NSObject {
         turtle.penUp()
         turtle.setPosition(to: Point(x: 250, y: 250))
         turtle.penDown()
-
+        
+        // Define the axiom
+        axiom = "UFFF[+B][-B]"
+        
+        // Generation 0
+        word = axiom
+        
+        // Re-write the word for two generations
+        for generation in 1...2 {
+            
+            // Create an empty new word
+            var newWord = ""
+            
+            // Write the new word based on the production rules
+            for character in word {
+                
+                switch character {
+                case "F":
+                    newWord.append("F")
+                case "B":
+                    newWord.append("FF[+B][-B]")
+                default:
+                    newWord.append(character)
+                }
+            }
+            
+            // Replace the old word with the new word
+            word = newWord
+            print("After generation \(generation) the word is:")
+            print(word)
+            
+        }
+        
+        // DEBUG:
+        print("Rendering:")
+                
     }
     
     // This function runs repeatedly, forever, to create the animated effect
@@ -32,9 +76,42 @@ class Sketch : NSObject {
         // Required to bring canvas into same orientation and origin position as last run of draw() function
         turtle.restoreStateOnCanvas()
 
-        // Move the turtle forward and turn it a bit
-        turtle.forward(steps: 5)
-        turtle.right(by: 1)
+        // Render the alphabet of the axiom
+        if canvas.frameCount < word.count {
+            
+            // Get an index for the current chracter in the axiom
+            let index = word.index(word.startIndex, offsetBy: canvas.frameCount)
+            let character = word[index]
+            
+            // DEBUG: What character is being rendered?
+            print(character, terminator: "")
+            
+            // Render based on this character
+            switch character {
+            case "F":
+                turtle.setPenColor(to: .black)
+                turtle.forward(steps: length)
+                print(turtle.currentHeading())
+            case "B":
+                turtle.forward(steps: length)
+                print(turtle.currentHeading())
+            case "+":
+                turtle.right(by: angle)
+                turtle.setPenColor(to: .green)
+            case "-":
+                turtle.left(by: angle)
+                turtle.setPenColor(to: .red)
+            case "[":
+                turtle.saveState()
+            case "]":
+                turtle.restoreState()
+            case "U":
+                turtle.left(by: 90)
+            default:
+                break
+            }
+            
+        }
         
     }
     
