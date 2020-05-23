c95b90920559f2d11f75d5232987fc260f913b98
Russell Gordon <rgordon@lcs.on.ca>
2020-05-22 20:31:44 -0400

    Abstracted logic for a Lindenmayer system into it's own structure.

---
+++ Animation/LindenmayerSystem.swift
@@ -0,0 +1,123 @@
+//
+//  LindenmayerSystem.swift
+//  Animation
+//
+//  Created by Russell Gordon on 2020-05-22.
+//  Copyright © 2020 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+import CanvasGraphics
+
+struct LindenmayerSystem {
+    
+    // Definition of system
+    let axiom: String
+    let length: Double
+    let angle: Degrees
+    let reduction: Double
+    let rules: [Character:String]
+    let generations: Int
+    let pointToStartRenderingFrom: Point
+
+    // Rendering state
+    var word: String = ""
+    var currentLength: Double = 0
+    
+    // Turtle to draw with
+    let t: Tortoise
+
+    init(axiom: String, length: Double, angle: Degrees, reduction: Double, rules: [Character:String], generations: Int, pointToStartRenderingFrom: Point, turtleToRenderWith: Tortoise) {
+        
+        self.axiom = axiom
+        self.length = length
+        self.angle = angle
+        self.reduction = reduction
+        self.rules = rules
+        self.generations = generations
+        self.pointToStartRenderingFrom = pointToStartRenderingFrom
+        self.t = turtleToRenderWith
+        
+        // Set up the system state
+        word = axiom
+        currentLength = length
+        
+        // Re-write the word for each generation
+        for generation in 1...generations {
+            
+            // Create an empty new word
+            var newWord = ""
+            
+            // Inspect each character of the old word
+            for character in word {
+                
+                // Iterate over all the rules
+                var match = false
+                for (key, value) in rules {
+                    
+                    // When a character matches, apply the rule
+                    if key == character {
+                        newWord.append(value)
+                        match = true
+                    }
+                    
+                }
+                
+                // If no match to rules, just copy the character to the new word
+                if match == false {
+                    newWord.append(character)
+                }
+                
+            }
+            
+            // Replace the old word with the new word
+            word = newWord
+            print("After generation \(generation) the word is:")
+            print(word)
+            
+            // Reduce the line length after generation 1
+            if generation > 1 {
+                currentLength /= reduction
+            }
+            
+        }
+
+    }
+    
+    // Render the next character of the system using the turtle provided
+    func update(forFrame currentFrame: Int) {
+        
+        // Required to bring canvas into same orientation and origin position as last run of draw() function
+        t.restoreStateOnCanvas()
+        
+        // Render the alphabet of the L-system
+        if currentFrame < word.count {
+            
+            // Get an index for the current chracter in the axiom
+            let index = word.index(word.startIndex, offsetBy: currentFrame)
+            let character = word[index]
+            
+            // DEBUG: What character is being rendered?
+            print(character, terminator: "")
+            
+            // Render based on this character
+            switch character {
+            case "S":
+                t.penUp()
+                t.setPosition(to: pointToStartRenderingFrom)
+                t.penDown()
+            case "F":
+                t.forward(steps: currentLength)
+            case "+":
+                t.right(by: angle)
+            case "-":
+                t.left(by: angle)
+            default:
+                break
+            }
+            
+        }
+        
+    }
+    
+}
--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -10,18 +10,9 @@ class Sketch : NSObject {
     // Tortoise to draw with
     let turtle: Tortoise
     
-    // L-system defintion
-    let axiom: String = "S-F"
-    let length: Double = 100
-    let angle: Degrees = 90
-    let reduction: Double = 3
-    let rules: [Character:String] = ["F":"F+F-F-F+F"]
-    let generations: Int = 4
-    let pointToStartRenderingFrom: Point = Point(x: 250, y: 100)
-    
-    // L-system interpreter state
-    var word: String
-    var currentLength: Double
+    // L-systems
+    let anotherKochConstruction: LindenmayerSystem
+
     
     // This function runs once
     override init() {
@@ -34,51 +25,17 @@ class Sketch : NSObject {
         
         // Create turtle to draw with
         turtle = Tortoise(drawingUpon: canvas)
-                
-        // Generation 0
-        word = axiom
-        currentLength = length
         
-        // Re-write the word for each generation
-        for generation in 1...generations {
-            
-            // Create an empty new word
-            var newWord = ""
-            
-            // Inspect each character of the old word
-            for character in word {
-                
-                // Iterate over all the rules
-                var match = false
-                for (key, value) in rules {
-                    
-                    // When a character matches, apply the rule
-                    if key == character {
-                        newWord.append(value)
-                        match = true
-                    }
-                    
-                }
-                
-                // If no match to rules, just copy the character to the new word
-                if match == false {
-                    newWord.append(character)
-                }
-                
-            }
-            
-            // Replace the old word with the new word
-            word = newWord
-            print("After generation \(generation) the word is:")
-            print(word)
-            
-            // Reduce the line length after generation 1
-            if generation > 1 {
-                currentLength /= reduction
-            }
-            
-        }
-                
+        // Create the system
+        anotherKochConstruction = LindenmayerSystem(axiom: "S-F",
+                                                    length: 100,
+                                                    angle: 90,
+                                                    reduction: 3,
+                                                    rules: ["F":"F+F-F-F+F"],
+                                                    generations: 4,
+                                                    pointToStartRenderingFrom: Point(x: 250, y: 100),
+                                                    turtleToRenderWith: turtle)
+                                        
         // DEBUG:
         print("Rendering:")
         
@@ -87,36 +44,8 @@ class Sketch : NSObject {
     // This function runs repeatedly, forever, to create the animated effect
     func draw() {
         
-        // Required to bring canvas into same orientation and origin position as last run of draw() function
-        turtle.restoreStateOnCanvas()
-        
-        // Render the alphabet of the L-system
-        if canvas.frameCount < word.count {
-            
-            // Get an index for the current chracter in the axiom
-            let index = word.index(word.startIndex, offsetBy: canvas.frameCount)
-            let character = word[index]
-            
-            // DEBUG: What character is being rendered?
-            print(character, terminator: "")
-            
-            // Render based on this character
-            switch character {
-            case "S":
-                turtle.penUp()
-                turtle.setPosition(to: pointToStartRenderingFrom)
-                turtle.penDown()
-            case "F":
-                turtle.forward(steps: currentLength)
-            case "+":
-                turtle.right(by: angle)
-            case "-":
-                turtle.left(by: angle)
-            default:
-                break
-            }
-            
-        }
+        // Update rendering of this system for the current frame of the animation
+        anotherKochConstruction.update(forFrame: canvas.frameCount)
         
     }
     
