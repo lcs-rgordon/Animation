7e549f8b88670019925045d61fcec310c687ad53
Russell Gordon <rgordon@lcs.on.ca>
2020-06-08 17:06:26 -0400

    Fix comments in initial sketch.

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -10,8 +10,9 @@ class Sketch: NSObject {
     //       Therefore, the line immediately below, or one like it, must always be present.
     var currentDrawing = BasicSketch()
 
-    // To try out other included sketches, uncomment one of the lines below.
-    // Then, uncomment one of the lines below.
+    // To try out other included sketches:
+    // 1. Comment out the code on line 11.
+    // 2. Uncomment one of the lines below.
 //    var currentDrawing = TurtleSketch()
 //    var currentDrawing = AudioInputSketch()
 //    var currentDrawing = StaticSketch()
